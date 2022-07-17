46c3555d7913ec1683e53a264a7f6450dc495eec
Lunixyz <109187405+Lunixyz@users.noreply.github.com>
2022-07-16 21:40:15 -0300

    Update report.js

--- C/report.js
+++ C/report.js
@@ -8,11 +8,7 @@ exports.run = async (client, message, args, prefix) => {
 
   try {
     client.channels.get('992172780848156763').send(
-      `bug/sugestão enviada: **
-          ${argumentos}
-           pelo usuário
-          ${message.author.username}
-          `
+      `bug/sugestão enviada: ${argumentos} pelo usuário ${message.author.username}`
     );
     message.reply(
       'Obrigado(a)! Pessoas como você ajudam no meu desenvolvimento! (por favor não abuse deste comando)'
